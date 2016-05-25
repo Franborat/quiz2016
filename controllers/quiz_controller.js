242d9d0f211bdd913365671fb8da4198e8b2203b
Francisco Losada <f.losadadelarosa@gmail.com>
2016-05-26 01:15:42 +0200

    2Intento:Sequelize.js y SQLite

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,40 @@
-/* GET /question. */
-exports.question = function(req, res, next){
-	var answer = req.query.answer || "";
-	res.render('quizzes/question', {question: 'Capital de Italia', answer:answer});
+var models = require('../models');
+
+// GET /question
+exports.question = function(req, res, next) {
+  models.Quiz.findOne() // Busca la primera pregunta existente en la tabla quiz
+    .then(function(quiz) { // Se pasa como un objeto isomorfico JS en el parametro quiz
+      if (quiz) {
+        var answer = req.query.answer || '';
+
+        res.render('quizzes/question', {question: quiz.question,
+                                answer: answer});
+        } else {
+          throw new Error('No hay preguntas en la BBDD.');
+        }
+    })
+    .catch(function(error) {
+      next(error);
+    });
 };
 
-/* GET /check. */
-exports.check = function(req, res, next){
-	
-    var answer = req.query.answer || "";
 
-	var result = req.query.answer === 'roma' ? 'Correcta' : 'Incorrecta';
-	res.render('quizzes/result', {result: result, answer: answer});
+// GET /check
+exports.check = function(req, res, next) {
+  models.Quiz.findOne() // Busca la primera pregunta existente
+    .then(function(quiz) {
+      if (quiz) {
+        var answer = req.query.answer || "";
+
+        var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
+
+        res.render('quizzes/result', { result: result, 
+                         answer: answer });
+        } else {
+          throw new Error('No hay preguntas en la BBDD.');
+        }
+    })
+    .catch(function(error) {
+      next(error);
+    });
 };
-
