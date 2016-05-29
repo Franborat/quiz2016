79459a66c2f3ae761aa342f085fd540256628a68
Francisco Losada <f.losadadelarosa@gmail.com>
2016-05-30 00:50:00 +0200

    Mensajes de Flash

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,6 @@
 var models = require('../models');
 
-
-// GET /quizzes
-exports.index = function(req, res, next) {
-	models.Quiz.findAll()
-		.then(function(quizzes) {
-			res.render('quizzes/index.ejs', { quizzes: quizzes});
-		})
-		.catch(function(error) {
-			next(error);
-		});
-};
-
-// Autoload el quiz asociado a quizId
+// Autoload el quiz asociado a :quizId
 exports.load = function(req, res, next, quizId) {
 	models.Quiz.findById(quizId)
   		.then(function(quiz) {
@@ -27,59 +15,40 @@ exports.load = function(req, res, next, quizId) {
 };
 
 
+// GET /quizzes
+exports.index = function(req, res, next) {
+	models.Quiz.findAll()
+		.then(function(quizzes) {
+			res.render('quizzes/index.ejs', { quizzes: quizzes});
+		})
+		.catch(function(error) {
+			next(error);
+		});
+};
+
 
 // GET /quizzes/:id
 exports.show = function(req, res, next) {
-	models.Quiz.findById(req.params.quizId)
-		.then(function(quiz) {
-			if (quiz) {
-				var answer = req.query.answer || '';
 
-				res.render('quizzes/show', {quiz: quiz,
-											answer: answer});
-			} else {
-		    	throw new Error('No existe ese quiz en la BBDD.');
-		    }
-		})
-		.catch(function(error) {
-			next(error);
-		});
+	var answer = req.query.answer || '';
+
+	res.render('quizzes/show', {quiz: req.quiz,
+								answer: answer});
 };
 
-// GET /quizzes/:texto_a_buscar/search
-exports.search = function(req, res) {
-	var texto_a_buscar = req.query.search;
-   models.Quiz.findAll({where: {question: {$like:"%texto_a_buscar%"}}})
-       .then(function(quizzes) {
-
-          res.render('quizzes/found.ejs', {quizzes:quizzes});
-
-       })
-       .catch(function(error) {
-			next(error);
-		});
-};
 
 // GET /quizzes/:id/check
-exports.check = function(req, res) {
-	models.Quiz.findById(req.params.quizId)
-		.then(function(quiz) {
-			if (quiz) {
-				var answer = req.query.answer || "";
+exports.check = function(req, res, next) {
 
-				var result = answer === quiz.answer ? 'Correcta' : 'Incorrecta';
+	var answer = req.query.answer || "";
 
-				res.render('quizzes/result', { quiz: quiz, 
-											   result: result, 
-											   answer: answer });
-			} else {
-				throw new Error('No existe ese quiz en la BBDD.');
-			}
-		})
-		.catch(function(error) {
-			next(error);
-		});	
+	var result = answer === req.quiz.answer ? 'Correcta' : 'Incorrecta';
+
+	res.render('quizzes/result', { quiz: req.quiz, 
+								   result: result, 
+								   answer: answer });
 };
+
 
 // GET /quizzes/new
 exports.new = function(req, res, next) {
@@ -92,12 +61,28 @@ exports.create = function(req, res, next) {
   var quiz = models.Quiz.build({ question: req.body.quiz.question, 
   	                             answer:   req.body.quiz.answer} );
 
-// guarda en DB los campos pregunta y respuesta de quiz
+  // guarda en DB los campos pregunta y respuesta de quiz
   quiz.save({fields: ["question", "answer"]})
   	.then(function(quiz) {
+		req.flash('success', 'Quiz creado con éxito.');
     	res.redirect('/quizzes');  // res.redirect: Redirección HTTP a lista de preguntas
     })
     .catch(function(error) {
+		req.flash('error', 'Error al crear un Quiz: '+error.message);
 		next(error);
 	});  
+};
+
+// GET /quizzes/:texto_a_buscar/search
+exports.search = function(req, res) {
+        var texto_a_buscar = req.query.search;
+   models.Quiz.findAll({where: {question: {$like:"%texto_a_buscar%"}}})
+       .then(function(quizzes) {
+
+          res.render('quizzes/found.ejs', {quizzes:quizzes});
+
+       })
+       .catch(function(error) {
+                        next(error);
+                });
 };
