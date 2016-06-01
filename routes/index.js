37c8a4e5d6a4b3757f46687a75a7b9da466c01e2
Francisco Losada <f.losadadelarosa@gmail.com>
2016-06-01 19:21:58 +0200

    Autores de Quizzes

--- routes/index.js
+++ routes/index.js
@@ -30,14 +30,15 @@ router.delete('/users/:userId(\\d+)',   userController.destroy);  // borrar cuen
 
 
 // Definición de rutas de /quizzes
+// Definición de rutas de /quizzes
 router.get('/quizzes',                     quizController.index);
 router.get('/quizzes/:quizId(\\d+)',       quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
-router.get('/quizzes/search', quizController.search);
+router.get('/quizzes/new',                 quizController.new);
 router.post('/quizzes',                    quizController.create);
 router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
 router.put('/quizzes/:quizId(\\d+)',       quizController.update);
-router.delete('/quizzes/:quizId(\\d+)', quizController.destroy);
+router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 
 router.get('/quizzes/:quizId(\\d+)/comments/new',  commentController.new);
 router.post('/quizzes/:quizId(\\d+)/comments',     commentController.create);
